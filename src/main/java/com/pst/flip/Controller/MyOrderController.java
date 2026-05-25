5fda3b2a46c360b7e78c010cd2f18e75664463cc
SyedYasin07 <syedyasin075@gmail.com>
2026-05-25 09:56:26 +0530

    updated

--- src/main/java/com/pst/flip/Controller/MyOrderController.java
+++ src/main/java/com/pst/flip/Controller/MyOrderController.java
@@ -5,25 +5,35 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.pst.flip.DAO.OrderDAO;
 import com.pst.flip.DTO.OrderDTO;
 
-
 @WebServlet("/myOrders")
 public class MyOrderController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int userId=1;
-		OrderDAO dao= new OrderDAO();
-		List<OrderDTO> orders= dao.getOrdersByUser(userId);
-		request.setAttribute("orders", orders);
-		request.getRequestDispatcher("myOrders.jsp").forward(request, response);
-	}
 
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int userId = 1;
+
+        System.out.println("USER ID = " + userId);
+
+        OrderDAO dao = new OrderDAO();
+
+        List<OrderDTO> orders = dao.getOrdersByUser(userId);
+
+        System.out.println("ORDERS SIZE = " + orders.size());
+
+        request.setAttribute("orders", orders);
+
+        request.getRequestDispatcher("myOrders.jsp")
+               .forward(request, response);
+    }
 }
