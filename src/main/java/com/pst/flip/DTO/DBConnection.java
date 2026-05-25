392b68008f9e8df30ebe8d06743f1c7a51205c9f
SyedYasin07 <syedyasin075@gmail.com>
2026-05-25 18:56:53 +0530

    db updated

--- src/main/java/com/pst/flip/DTO/DBConnection.java
+++ src/main/java/com/pst/flip/DTO/DBConnection.java
@@ -1,3 +1,36 @@
+
+// package com.pst.util.DB;
+
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+
+// public class DBConnection {
+
+//     public static Connection getConnection() {
+//         Connection con = null;
+//         try {
+//             Class.forName("com.mysql.cj.jdbc.Driver");
+
+//             con = DriverManager.getConnection(
+//             	    "jdbc:mysql://shuttle.proxy.rlwy.net:39720/flip?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+//             	    "root",
+//             	    "awsYxCKkaHAtWDuAftCnfuxWTpnXFruL"
+//             	);
+
+//             System.out.println("✅ MySQL DB connected successfully");
+
+//         } catch (ClassNotFoundException e) {
+//             System.err.println("❌ MySQL JDBC Driver not found");
+//             e.printStackTrace();
+//         } catch (SQLException e) {
+//             System.err.println("❌ Failed to connect to MySQL database");
+//             e.printStackTrace();
+//         }
+//         return con;
+//     }
+// }
+
 package com.pst.flip.DTO;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -5,17 +38,25 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     public static Connection getConnection() {
+
         Connection con = null;
+
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
+
             con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/flip?useSSL=false&serverTimezone=UTC",
+                "jdbc:mysql://shuttle.proxy.rlwy.net:39720/flip?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
                 "root",
-                "$yasin4758"
+                "awsYxCKkaHAtWDuAftCnfuxWTpnXFruL"
             );
+
+            System.out.println("✅ DB Connected");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return con;
     }
 }
