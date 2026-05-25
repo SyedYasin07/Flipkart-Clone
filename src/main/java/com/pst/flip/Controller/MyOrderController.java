ef6cc36df23609e8e53c6a7329666a330727e85f
SyedYasin07 <syedyasin075@gmail.com>
2026-05-25 20:21:04 +0530

    changed

---
+++ src/main/java/com/pst/flip/Controller/MyOrderController.java
@@ -0,0 +1,32 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+package com.pst.flip.Controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.pst.flip.DAO.OrderDAO;
+import com.pst.flip.DTO.OrderDTO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet({"/myOrders"})
+public class MyOrderController extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public MyOrderController() {
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      int userId = 1;
+      System.out.println("USER ID = " + userId);
+      OrderDAO dao = new OrderDAO();
+      List<OrderDTO> orders = dao.getOrdersByUser(userId);
+      System.out.println("ORDERS SIZE = " + orders.size());
+      request.setAttribute("orders", orders);
+      request.getRequestDispatcher("myOrders.jsp").forward(request, response);
+   }
+}
